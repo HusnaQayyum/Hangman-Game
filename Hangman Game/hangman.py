33d185fbdedab86d847083bb4d746e8767b8ae25
HusnaQayyum <laibaqayyym739@gmail.com>
2025-12-31 10:59:17 +0500

    1st commit

---
+++ Hangman Game/hangman.py
@@ -0,0 +1,32 @@
+import random
+
+words = ["apple", "mango", "pineapple", "watermelon", "grapes"]
+
+secret_word = random.choice(words)
+guessed_word = ["_"] * len(secret_word)
+
+attempts = 6
+
+print("Welcome to Hangman Game!")
+print(" ".join(guessed_word))
+
+while attempts > 0:
+    guess = input("Guess a letter: ")
+
+    if guess in secret_word:
+        for i in range(len(secret_word)):
+            if secret_word[i] == guess:
+                guessed_word[i] = guess
+    else:
+        attempts -= 1
+
+    print(" ".join(guessed_word))
+    print("Attempts left:", attempts)
+
+    if "_" not in guessed_word:
+        break
+
+if "_" not in guessed_word:
+    print("You Win! The word was:", secret_word)
+else:
+    print("Game Over! The word was:", secret_word)
